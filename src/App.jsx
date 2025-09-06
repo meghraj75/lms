216f167db40929aa87e2cd198a42d762bf883a78
amdadislam01 <amdadislam733@gmail.com>
2025-09-06 12:54:02 +0600

    learning-management-system project setup and Navbar complate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Navbar from './components/Navbar';
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
